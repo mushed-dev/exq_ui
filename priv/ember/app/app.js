a4cad5e148a4ca806c03bd0196d9175ca5156f04
anderegg <daniel.anderegg@xledger.com>
2016-12-09 16:16:25 -0700

    built

--- priv/ember/app/app.js
+++ priv/ember/app/app.js
@@ -1,16 +1,16 @@
 import Ember from 'ember';
-import Resolver from 'ember-resolver';
+import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
-// sets up Ember.Inflector
-import './models/custom-inflector-rules';
+
+let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-var App = Ember.Application.extend({
+App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
